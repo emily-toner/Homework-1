fd286a778461c0430193efbc3526cae39ad37d73
root <root@EmilyUConn.localdomain>
2025-09-03 10:16:45 -0400

    Add AdvancedMath.h and MathUtils.h

---
+++ homework/homework_one/include/MathUtils.h
@@ -0,0 +1,9 @@
+#ifndef MATHUTILS
+#define MATHUTILS
+
+namespace MathUtils {
+	int add(int a, int b);
+	int multiply( int a, int b);
+
+}
+#endif
---
+++ homework/homework_one/src/AdvancedMath.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "MathUtils.h"                                                                  
+#include "AdvancedMath.h"
+
+namespace AdvancedMath {
+	int square(int n) {
+		return MathUtils::multiply(n, n);
+	}
+}
