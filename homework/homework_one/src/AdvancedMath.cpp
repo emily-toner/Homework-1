4739e8413633a287555072b4f6fee137adf7d0aa
emily-toner <emilyatoner@gmail.com>
2025-09-25 10:59:18 -0400

    Delete homework/homework_one directory

--- homework/homework_one/src/AdvancedMath.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "MathUtils.h"                                                                  
-#include "AdvancedMath.h"
-
-namespace AdvancedMath {
-	int square(int n) {
-		return MathUtils::multiply(n, n);
-	}
-}
